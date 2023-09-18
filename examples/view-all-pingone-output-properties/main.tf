8827e3daaa5ca5e1d01ae31373729562a911d712
Patrick Cowland <patrickcowland@pingidentity.com>
2023-09-18 14:49:27 +0100

    update version

--- examples/view-all-pingone-output-properties/main.tf
+++ examples/view-all-pingone-output-properties/main.tf
@@ -1,6 +1,6 @@
 module "pingone_utils" {
   source  = "pingidentity/utils/pingone"
-  version = "0.0.7"
+  version = "0.0.8"
 
   region         = pingone_environment.my_environment.region
   environment_id = pingone_environment.my_environment.id
