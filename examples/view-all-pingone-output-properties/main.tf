cc462e49fa880f7e70c6f67f098a11d7f88707cc
Patrick Cowland <patrickcowland@pingidentity.com>
2023-09-18 14:52:10 +0100

    reversion

--- examples/view-all-pingone-output-properties/main.tf
+++ examples/view-all-pingone-output-properties/main.tf
@@ -1,6 +1,6 @@
 module "pingone_utils" {
   source  = "pingidentity/utils/pingone"
-  version = "0.0.8"
+  version = ">= 0.0.6"
 
   region         = pingone_environment.my_environment.region
   environment_id = pingone_environment.my_environment.id
--- scripts/tfupdate-examples.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-tfupdate module -r -v $(cat .version) pingidentity/utils/pingone ./examples
